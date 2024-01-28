ea6abd288e43cd249763d450a440544cfc694cf9
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-01-28 20:53:09 +0100

    annee30

--- TP4_exo1/shaders/grey2d.fs.glsl
+++ TP4_exo1/shaders/grey2d.fs.glsl
@@ -1,9 +1,11 @@
 #version 330 core
 
 in vec3 vFragColor;
+float Color_grey = (vFragColor.r + vFragColor.g + vFragColor.b) / 3.0;
 
 out vec4 fFragColor;
 
 void main() {
-  fFragColor = vec4(vFragColor, 1.);
-};
+    fFragColor = vec4(Color_grey, Color_grey, Color_grey, 1.);
+
+}
