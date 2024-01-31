af273d4dec1cee3f2d48965a9da183c7444dbd39
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-01-31 14:01:33 +0100

    vbo

---
+++ TP4_exo2/main.cpp
@@ -0,0 +1,17 @@
+// class VBO{
+//     public:
+//         VBO(){
+//             glGenBuffers(1, &_id); 
+//         }; 
+//         ~VBO(){
+//             glDeleteBuffers(1, &_id);
+//         }
+
+//         void bind() const {
+//             glBindBuffer(GL_ARRAY_BUFFER, id); 
+//         }
+//     private:
+//     GLuint _id{}; 
+
+
+// }; 
